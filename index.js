8816d467a85134ca5ebc3e76df835cb2ea85566b
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-01-15 11:43:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    console.log(`This Saturday, I want to ${activity} !`)
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+function mondayWork(activity ="go to the office"){
+    console.log(`This Monday, I will ${activity} !`)
+}
+mondayWork()
+mondayWork("work from home")
+function wrapAdjective(special = "*") {
+    const emphatic = function (adjective = "a hard worker") {
+        return `You are${special} ${adjective}`;
+    };
+    return emphatic;
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
